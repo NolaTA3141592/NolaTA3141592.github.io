25015616eb9e142c70cf2dc83ddffc4ef25b5a7b
Noe <rei11a2004@gmail.com>
2023-09-22 18:24:32 +0900

    init

---
+++ base/src/HEAD.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HEAD.css'
+const HEAD = () => {
+    return (
+        <header className='HEAD-sticky-header'>
+            <h1 className='HEAD-word'>
+                Nola | 開発中
+            </h1>
+        </header>
+    )
+}
+
+export default HEAD
